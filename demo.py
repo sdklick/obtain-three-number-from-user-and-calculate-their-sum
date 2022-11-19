5134f28a96d84ac340f5ca01c5fb8c26272be391
Sumanta Das <sumantad827@gmail.com>
2022-11-19 10:43:31 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+# write a python program to obtain three number from user and calculate their sum
+
+userinput1 = int(input("Enter 1st number : "))
+userinput2 = int(input("Enter 2nd number : "))
+userinput3 = int(input("Enter 3rd number : "))
+
+sumcalculate = userinput1+userinput2+userinput3
+
+print("you are enter", userinput1, "+", userinput2, "+", userinput3)
+
+print("sum these three number are : ", sumcalculate)
